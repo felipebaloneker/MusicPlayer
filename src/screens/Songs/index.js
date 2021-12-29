7177baad4398cf52ccd1f4a0f010ddad94c6634f
Felipe <felipeBaloneker23@gmail.com>
2021-12-28 23:45:13 -0300

    rendering playing tracks

--- src/screens/Songs/index.js
+++ src/screens/Songs/index.js
@@ -2,10 +2,27 @@ import React,{Component} from "react";
 import {View,Text, ScrollView} from 'react-native'
 import { styles } from './styles'
 import { AudioContext } from '../../context/AudioList'
+import { Audio }from 'expo-av'
 
 export class Songs extends Component{
     static contextType = AudioContext
+    constructor(props){
+        super(props);
+        this.state={
+            playbackObject:null,
+            soundObject:null,
+        }
+        this.currentItem = {}
+    }
 
+    trackPlay = async (track)=>{
+        console.log(track)
+        const playbackObject = new Audio.Sound();
+        playbackObject.loadAsync(
+            {uri: track.uri},
+            {shouldPlay:true}
+        )
+    }
     render(){
         return(
             <ScrollView style={styles.container}>
@@ -15,7 +32,7 @@ export class Songs extends Component{
                 >
                     <Text style={styles.text} 
                     key={track.id}
-                    onPress={()=>{console.log('pressed')}}
+                    onPress={()=>this.trackPlay(track)}
                     >
                         {track.filename}
                     </Text> 
